e9fca0e1161d77aa74f3709f4005842e71c0f7fb
Ivan Ulloque <95107566+ZUR1C4T0@users.noreply.github.com>
2024-03-25 11:07:38 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter, type NestFastifyApplication } from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(),
+	);
+
+	// enable cors
+	app.enableCors();
+
+	// use pipes
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+		}),
+	);
+
+	// start the server
+	const port = +process.env.PORT || 3000;
+	const address = "0.0.0.0";
+	await app.listen(port, address);
+}
+bootstrap();
