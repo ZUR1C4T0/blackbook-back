23f79cfcee2ff3d3df8693819318713d6fa8637a
ZUR1C4T0 <ivanulloque19@gmail.com>
2024-06-29 22:30:02 -0500

    Move local authentication guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Inject, Post, Req, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
 import type { User } from "@prisma/client";
 import type { Request } from "express";
 import { AuthService } from "./auth.service";
 import { ApiTags } from "@nestjs/swagger";
+import { LocalAuthGuard } from "./guards/local-auth.guard";
 
 declare module "express" {
 	interface Request {
@@ -18,7 +18,7 @@ export class AuthController {
 	private readonly authService: AuthService;
 
 	@Post("login")
-	@UseGuards(AuthGuard("local"))
+	@UseGuards(LocalAuthGuard)
 	async login(@Req() req: Request) {
 		const user = req.user;
 		return this.authService.generateToken(user);
